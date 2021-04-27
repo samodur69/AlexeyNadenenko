c017576faeee47f3e635be785708faececfecbc6
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-04-27 10:34:55 +0300

    Division test with long and doubles done

---
+++ src/test/java/ru/training/at/hw1/CalculatorDivisionTest.java
@@ -0,0 +1,79 @@
+package ru.training.at.hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.training.at.hw1.dataproviders.DataProviders;
+
+public class CalculatorDivisionTest {
+
+    Calculator calculator;
+
+    @BeforeClass
+    public void calcSetUp() {
+        calculator = new Calculator();
+    }
+
+    @Test(description = "regular division test",
+            dataProviderClass = DataProviders.class,
+            dataProvider = "correct Long Numbers")
+    public void regularLongDivTest(long a, long b) {
+        long actual = calculator.div(a, b);
+        long expected = a / b;
+        Assert.assertEquals(actual, expected, "incorrect regular division with long numbers");
+    }
+
+    @Test(expectedExceptions = NumberFormatException.class,
+            description = "division by Zero test",
+            dataProviderClass = DataProviders.class,
+            dataProvider = "long numbers for division by zero")
+    public void divisionLongByZeroTest(long a, long b) {
+        long actual = calculator.div(a, b);
+        long expected = a / b;
+        Assert.assertEquals(actual, expected, "Wrong division by Zero");
+    }
+
+    @Test(description = "zero by number division test",
+            dataProvider = "for division zero by long numbers",
+            dataProviderClass = DataProviders.class)
+    public void zeroByLongDivTest(long a, long b) {
+        long actual = calculator.div(a, b);
+        long expected = 0;
+        Assert.assertEquals(actual, expected, "Incoorect division zero by number");
+    }
+
+    @Test(description = "regular division test with double numbers",
+            dataProviderClass = DataProviders.class,
+            dataProvider = "correct Double Numbers")
+    public void regularDoubleDivTest(double a, double b) {
+        double actual = calculator.div(a, b);
+        double expected = a / b;
+        Assert.assertEquals(actual, expected, "incorrect regular division with double numbers");
+    }
+
+    @Test(description = "division double by Zero test",
+            dataProviderClass = DataProviders.class,
+            dataProvider = "double numbers for division by zero")
+    public void divisionDoubleByZeroTest(double a, double b) {
+        double actual = calculator.div(a, b);
+        double expected = a / b;
+        Assert.assertEquals(actual, expected, "Wrong division double by Zero");
+    }
+
+    @Test(description = "zero by double number division test",
+            dataProvider = "for division zero by double numbers",
+            dataProviderClass = DataProviders.class)
+    public void zeroByDoubleDivTest(double a, double b) {
+        double actual = calculator.div(a, b);
+        double expected = 0;
+        Assert.assertEquals(actual, expected, "Incorrect division zero by double");
+    }
+
+
+    @AfterClass
+    public void calcTearDown() {
+        calculator = null;
+    }
+}
