a5d5e82d00b73d2956296976edf0abdd803fecee
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-04-27 13:55:29 +0300

    add sqrt test and found mistake in calculator. Sqrt of numbers less than 0 must produce error or NaN

---
+++ src/test/java/ru/training/at/hw1/CalculatorSqrtTest.java
@@ -0,0 +1,32 @@
+package ru.training.at.hw1;
+
+import com.epam.tat.module4.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.training.at.hw1.dataproviders.DataProviders;
+
+public class CalculatorSqrtTest {
+
+    Calculator calculator;
+
+    @BeforeClass
+    public void calcSetUp() {
+        calculator = new Calculator();
+    }
+
+    @Test(description = "sqrt test",
+            dataProviderClass = DataProviders.class,
+            dataProvider = "sqrt double numbers")
+    public void sqrtTest(double a) {
+        double result = calculator.sqrt(a);
+        double expected = Math.sqrt(a);
+        Assert.assertEquals(result, expected, "sqrt incorrect. If num<0 result must be NaN");
+    }
+
+    @AfterClass
+    public void calcTearDown() {
+        calculator = null;
+    }
+}
