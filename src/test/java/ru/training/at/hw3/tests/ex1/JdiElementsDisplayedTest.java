83cd1019903cd4eb9442003b1ad58b4229a4b62d
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-12 16:19:08 +0300

    finish test ex1

--- src/test/java/ru/training/at/hw3/tests/ex1/JdiElementsDisplayedTest.java
+++ src/test/java/ru/training/at/hw3/tests/ex1/JdiElementsDisplayedTest.java
@@ -1,56 +1,31 @@
-package ru.training.at.hw3.ex1;
+package ru.training.at.hw3.tests.ex1;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import ru.training.at.hw3.Utils.DriverManager;
 import ru.training.at.hw3.page_object.JdiMainPage;
-
-import java.io.FileInputStream;
-import java.io.IOException;
+import ru.training.at.hw3.tests.BaseTest;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class JdiElementsDisplayedTest {
-
-    WebDriver driver;
-    JdiMainPage mainPage;
-    Properties prop;
-
-    @BeforeClass (alwaysRun = true)
-    public void browserSetUp() {
-        driver = DriverManager.setupDriver();
-        try {
-            FileInputStream fi =
-                    new FileInputStream("src/test/java/ru/training/at/hw3/testData.properties");
-            prop = new Properties();
-            prop.load(fi);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class JdiElementsDisplayedTest extends BaseTest {
 
     @Test
-    public void openPageAndCheckTitle() {
+    public void openPageAndCheckTitleTest() {
         mainPage = new JdiMainPage();
         mainPage.openPage();
         assertTrue(mainPage.checkTitle(prop.getProperty("homepage_title")), "wrong title");
     }
 
-    @Test (dependsOnMethods = "openPageAndCheckTitle")
-    public void loginAndCheckLoggedUser() {
+    @Test (dependsOnMethods = "openPageAndCheckTitleTest")
+    public void loginAndCheckLoggedUserTest() {
         mainPage.logIn(prop.getProperty("LOGIN"), prop.getProperty("PSW"));
         assertEquals(mainPage.getLoggedUserName(), prop.getProperty("USERNAME"));
     }
 
-    @Test (dependsOnMethods = "openPageAndCheckTitle",
-    enabled = false)
-    public void checkHeaderMenuItems() {
+    @Test (dependsOnMethods = "openPageAndCheckTitleTest")
+    public void checkHeaderMenuItemsTest() {
         List<String> expectedHeaderMenu = Arrays
                 .asList(prop.getProperty("MENU_0"),
                         prop.getProperty("MENU_1"),
@@ -58,12 +33,14 @@ public class JdiElementsDisplayedTest {
                         prop.getProperty("MENU_3"));
         expectedHeaderMenu.replaceAll(String::toUpperCase);
 
-        assertTrue(mainPage.compareLeftMenuItems(expectedHeaderMenu),
-                "header menu items incorrect");
+        listElementsComparator(
+                mainPage
+                .getHeaderMenu()
+                .getHeaderMenuItems(), expectedHeaderMenu, "header menu items");
     }
 
-    @Test (dependsOnMethods = "openPageAndCheckTitle")
-    public void checkBenefitsIconsAndTexts() {
+    @Test (dependsOnMethods = "openPageAndCheckTitleTest")
+    public void checkBenefitsIconsAndTextsTest() {
         assertEquals(mainPage.countBenefitIcons(), 4,
                 "wrong amount of benefits icons");
         List<String> benefitTexts = Arrays
@@ -71,30 +48,25 @@ public class JdiElementsDisplayedTest {
                         prop.getProperty("BENEFIT_1"),
                         prop.getProperty("BENEFIT_2"),
                         prop.getProperty("BENEFIT_3"));
-
-        assertTrue(mainPage.checkBenefitTexts(benefitTexts), "wrong benefits texts");
+        listElementsComparator(mainPage
+                .getBenefitsTexts(), benefitTexts, "benefit text");
     }
 
-    @Test (dependsOnMethods = "openPageAndCheckTitle")
-    public void checkFrameExistSwitchToItAndBack() {
+    @Test (dependsOnMethods = "openPageAndCheckTitleTest")
+    public void checkFrameExistSwitchToItAndBackTest() {
         //TODO change frame switch logic
         assertTrue(mainPage.checkFrameIsDisplayed(), "expected frame does not exist");
         assertTrue(mainPage.switchToFrameAndCheckButton(), "expected button does not exist");
     }
 
-    @Test (dependsOnMethods = "openPageAndCheckTitle")
-    public void checkLeftMenuItems() {
+    @Test (dependsOnMethods = "openPageAndCheckTitleTest")
+    public void checkLeftMenuItemsTest() {
         List<String> expected = Arrays
                 .asList(prop.getProperty("MENU_0"),
                         prop.getProperty("MENU_1"),
                         prop.getProperty("MENU_2"),
                         prop.getProperty("MENU_3"),
                         prop.getProperty("MENU_4"));
-        assertTrue(mainPage.compareLeftMenuItems(expected), "left menu incorrect");
-    }
-
-    @AfterClass (alwaysRun = true)
-    public void browserTearDown() {
-        DriverManager.closeBrowser();
+        listElementsComparator(mainPage.getLeftMenu().getLeftMenuItems(), expected, "left menu");
     }
 }
