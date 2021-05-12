26e1d6c2d106b2e0270b527d942123566e2394f5
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-12 09:41:54 +0300

    add singleton driver manager

---
+++ src/test/java/ru/training/at/hw3/DriverManager.java
@@ -0,0 +1,22 @@
+package ru.training.at.hw3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public final class DriverManager {
+
+    public static WebDriver driver;
+
+    public static WebDriver setupDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        }
+        return driver;
+    }
+}
