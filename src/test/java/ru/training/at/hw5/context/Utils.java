1eb5a7db5142230307fa2b82f0001bb93e4f03b6
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-19 10:49:05 +0300

    regex generator util

--- src/test/java/ru/training/at/hw5/context/Utils.java
+++ src/test/java/ru/training/at/hw5/context/Utils.java
@@ -9,15 +9,10 @@ public class Utils {
 
     public static List<String> generateRegex(Map<String, String> expected) {
         List<String> regExp = new ArrayList<>();
-        for(Map.Entry<String, String> el : expected.entrySet()) {
-            String regex = new StringBuilder(".*(")
-                    .append(el.getKey())
-                    .append(").*(")
-                    .append(el.getValue())
-                    .append(")")
-                    .toString();
+        expected.forEach((key, value) -> {
+            String regex = ".*(" + key + ").*(" + value + ")";
             regExp.add(regex);
-        }
+        });
         // skip fist element because it is table header
         return regExp.stream().skip(1).collect(Collectors.toList());
     }
