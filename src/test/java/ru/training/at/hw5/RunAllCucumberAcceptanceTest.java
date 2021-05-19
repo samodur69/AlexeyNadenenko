764e5d61a6bda4bcf28336528c1febc4998d36a3
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-19 10:47:12 +0300

    add profile for hw5

--- src/test/java/ru/training/at/hw5/RunAllCucumberAcceptanceTest.java
+++ src/test/java/ru/training/at/hw5/RunAllCucumberAcceptanceTest.java
@@ -3,7 +3,9 @@ package ru.training.at.hw5;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions (features = {"src/test/resources/cucumber_features"}, tags = "@TestTag and @TestTag1")
+@CucumberOptions (
+        features = {"src/test/resources/cucumber_features"},
+        tags = "@TestTag")
 public class RunAllCucumberAcceptanceTest extends AbstractTestNGCucumberTests {
 
 }
