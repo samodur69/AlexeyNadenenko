5888092f506ab9f81e51d012a9c4ad1020017b1f
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-19 10:49:45 +0300

    fix method click Header menu

--- src/main/java/ru/training/at/hw5/pageobjects/JdiHeaderMenu.java
+++ src/main/java/ru/training/at/hw5/pageobjects/JdiHeaderMenu.java
@@ -16,9 +16,9 @@ public class JdiHeaderMenu extends BasePage {
     @FindBy(xpath = "//*[@class='uui-navigation nav navbar-nav m-l8']/child::li")
     List<WebElement> headerTopLevelMenu;
 
-    //    @FindBy(linkText = "SERVICE")
-    @FindBy(xpath = "//a[@class='dropdown-toggle']")
-    WebElement headerSubMenuOpener;
+    //    //    @FindBy(linkText = "SERVICE")
+    //    @FindBy(xpath = "//a[@class='dropdown-toggle']")
+    //    WebElement headerSubMenuOpener;
 
     @FindBy(xpath = "//*[@class='dropdown-menu']/child::li")
     List<WebElement> headerSubMenu;
@@ -36,18 +36,17 @@ public class JdiHeaderMenu extends BasePage {
     }
 
     public void clickHeaderMenuButton(String menuItem) {
-        headerSubMenuOpener.click();
-        //        for (WebElement el : headerTopLevelMenu) {
-        //            if (el.getText().equals(menuItem)) {
-        //                el.click();
-        //                break;
-        //            }
-        //        }
+        //        headerSubMenuOpener.click();
+        for (WebElement el : headerTopLevelMenu) {
+            if (el.getText().equals(menuItem.toUpperCase(Locale.ROOT))) {
+                el.click();
+                break;
+            }
+        }
     }
 
     public void clickHeaderSubMenu(String item) {
         for (WebElement el : headerSubMenu) {
-            System.out.println(el.getText());
             if (el.getText().equals(item.toUpperCase(Locale.ROOT))) {
                 el.click();
                 break;
@@ -55,33 +54,33 @@ public class JdiHeaderMenu extends BasePage {
         }
     }
 
-//    public void clickHeaderServiceMenuItem(String item) {
-//        headerSubMenuOpener.click();
-//        headerSubMenu.forEach(s -> System.out.println(s.getText()));
-//        new WebDriverWait(driver, 10)
-//                .until(ExpectedConditions.visibilityOfAllElements(headerSubMenu));
-//        for (WebElement el : headerSubMenu) {
-//            if (el.getText().equals(item.toUpperCase(Locale.ROOT))) {
-//                el.click();
-//                break;
-//            }
-//        }
-//    }
+    //    public void clickHeaderServiceMenuItem(String item) {
+    //        headerSubMenuOpener.click();
+    //        headerSubMenu.forEach(s -> System.out.println(s.getText()));
+    //        new WebDriverWait(driver, 10)
+    //                .until(ExpectedConditions.visibilityOfAllElements(headerSubMenu));
+    //        for (WebElement el : headerSubMenu) {
+    //            if (el.getText().equals(item.toUpperCase(Locale.ROOT))) {
+    //                el.click();
+    //                break;
+    //            }
+    //        }
+    //    }
 
-//    public List<String> getHeaderMenuItems() {
-//        return getTextFromVisibleElements(headerTopLevelMenu);
-//    }
-//
-//    @Step(value = "Click in header to {item} menu item")
-//    public void clickHeaderServiceMenuItem(String item) {
-//        headerSubMenuOpener.click();
-//        new WebDriverWait(driver, 10)
-//                .until(ExpectedConditions.visibilityOfAllElements(headerSubMenu));
-//        for (WebElement el : headerSubMenu) {
-//            if (el.getText().equals(item)) {
-//                el.click();
-//                break;
-//            }
-//        }
-//    }
+    //    public List<String> getHeaderMenuItems() {
+    //        return getTextFromVisibleElements(headerTopLevelMenu);
+    //    }
+    //
+    //    @Step(value = "Click in header to {item} menu item")
+    //    public void clickHeaderServiceMenuItem(String item) {
+    //        headerSubMenuOpener.click();
+    //        new WebDriverWait(driver, 10)
+    //                .until(ExpectedConditions.visibilityOfAllElements(headerSubMenu));
+    //        for (WebElement el : headerSubMenu) {
+    //            if (el.getText().equals(item)) {
+    //                el.click();
+    //                break;
+    //            }
+    //        }
+    //    }
 }
