f7bf635459631d0d13bfa83f020ba601cf14c8eb
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-21 14:38:16 +0300

    fix issue: delete commented functions, fields, locators

--- src/main/java/ru/training/at/hw5/pageobjects/JdiHeaderMenu.java
+++ src/main/java/ru/training/at/hw5/pageobjects/JdiHeaderMenu.java
@@ -1,13 +1,9 @@
 package ru.training.at.hw5.pageobjects;
 
-import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 import java.util.Locale;
 
@@ -15,10 +11,6 @@ public class JdiHeaderMenu extends BasePage {
 
     @FindBy(xpath = "//*[@class='uui-navigation nav navbar-nav m-l8']/child::li")
     List<WebElement> headerTopLevelMenu;
-
-    //    //    @FindBy(linkText = "SERVICE")
-    //    @FindBy(xpath = "//a[@class='dropdown-toggle']")
-    //    WebElement headerSubMenuOpener;
 
     @FindBy(xpath = "//*[@class='dropdown-menu']/child::li")
     List<WebElement> headerSubMenu;
@@ -53,34 +45,4 @@ public class JdiHeaderMenu extends BasePage {
             }
         }
     }
-
-    //    public void clickHeaderServiceMenuItem(String item) {
-    //        headerSubMenuOpener.click();
-    //        headerSubMenu.forEach(s -> System.out.println(s.getText()));
-    //        new WebDriverWait(driver, 10)
-    //                .until(ExpectedConditions.visibilityOfAllElements(headerSubMenu));
-    //        for (WebElement el : headerSubMenu) {
-    //            if (el.getText().equals(item.toUpperCase(Locale.ROOT))) {
-    //                el.click();
-    //                break;
-    //            }
-    //        }
-    //    }
-
-    //    public List<String> getHeaderMenuItems() {
-    //        return getTextFromVisibleElements(headerTopLevelMenu);
-    //    }
-    //
-    //    @Step(value = "Click in header to {item} menu item")
-    //    public void clickHeaderServiceMenuItem(String item) {
-    //        headerSubMenuOpener.click();
-    //        new WebDriverWait(driver, 10)
-    //                .until(ExpectedConditions.visibilityOfAllElements(headerSubMenu));
-    //        for (WebElement el : headerSubMenu) {
-    //            if (el.getText().equals(item)) {
-    //                el.click();
-    //                break;
-    //            }
-    //        }
-    //    }
 }
