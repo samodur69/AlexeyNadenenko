d66403ef580e787d370c3e3088a94336a6fee87f
Alexey Nadenenko <Alexey_Nadenenko1@epam.com>
2021-05-24 10:39:50 +0300

    hw6 - done

--- src/test/java/ru/training/at/hw6/test/BaseTest.java
+++ src/test/java/ru/training/at/hw6/test/BaseTest.java
@@ -6,18 +6,16 @@ import org.testng.annotations.BeforeSuite;
 import ru.training.at.hw6.site.JdiSite;
 
 import static com.epam.jdi.light.driver.WebDriverUtils.killAllSeleniumDrivers;
-import static com.epam.jdi.light.elements.composite.WebPage.openSite;
 
 public interface BaseTest {
 
     @BeforeSuite(alwaysRun = true)
     default void setUp() {
         PageFactory.initSite(JdiSite.class);
-//        openSite(JdiSite.class);
     }
 
     @AfterSuite(alwaysRun = true)
-    default void tearDowm() {
+    default void tearDown() {
         killAllSeleniumDrivers();
     }
 }
